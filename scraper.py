c388113b24c652e8bb3224a7333dd79e6b74277c
KFCMike <39376007+KFCMike@users.noreply.github.com>
2018-06-26 19:40:42 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,10 +16,10 @@ waitime=[j['result']['hospData'][i]['topWait'] for i in range(0,18)]
 
 names[15]=names[15][0:21]
 
-dt=j['result']['hospData'][0]['hospTimeEn']
+
 
 data = {names[i]: waitime[i] for i in range(0, 18)}
-
+dt=j['result']['hospData'][0]['hospTimeEn']
 data['date'] = dt
 
 scraperwiki.sqlite.save(unique_keys=['date'], data=data)
